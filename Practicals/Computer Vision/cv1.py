7ce6771a55982b73323d94ab4cc9e5ee0015bfb3
RAMAR BOSE <83829394+ramar92@users.noreply.github.com>
2023-08-02 14:25:45 +0530

    Delete Practicals directory

--- Practicals/Computer Vision/cv1.py
+++
@@ -1,5 +0,0 @@
-import cv2  
-img = cv2.imread('G:\Computer_Vision\ganapathi.jpg',1)  
-cv2.imshow('image',img)  
-cv2.waitKey(0)   
-cv2.destroyAllWindows()
--- Practicals/Computer Vision/edgedetection.py
+++
@@ -1,8 +0,0 @@
-import cv2  
-img = cv2.imread(r'G:\Computer_Vision\cat.png',1)  
-edges = cv2.Canny(img, 100, 200)  
-  
-cv2.imshow("Edge Detected Image", edges)  
-cv2.imshow("Original Image", img)  
-cv2.waitKey(0)   
-cv2.destroyAllWindows()  
